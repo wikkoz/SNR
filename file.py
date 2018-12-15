306cf4d64e6b1084d027b9d347c81e2b3d27deab
wikkoz <you@example.com>
2018-12-15 19:30:23 +0100

    Reading images with fft

--- file.py
+++ file.py
@@ -7,14 +7,21 @@ import numpy as np
 
 def readTrainingSet():
     training = 'fruits-360/Test'
-    images = {}
+    images = []
     for className in listdir(training):
-        classImages = []
         classDir = training + '/' + className
         for filename in listdir(classDir):
             path = classDir + '/' + filename
-            classImages.append(ndimage.imread(path))
-        images[className] = classImages
-    print (images)
+            images.append((transformImage(ndimage.imread(path)), className))
+
+def transformImage(image):
+    result = np.zeros((100, 100, 6))
+    for i in range(3):
+        fft = np.fft.fft2(image[:, :, i])
+        fshift = np.fft.fftshift(fft)
+        result[:, :, 2 * i] = fft.real
+        result[:, :, 2 * i + 1] = fft.imag
+    return result
+
 
 readTrainingSet()
