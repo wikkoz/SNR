cabf2e9d931cf6dee385567943d30a8f45a05c7e
wikkoz <you@example.com>
2018-12-16 11:18:26 +0100

    Model version 1

--- file.py
+++ file.py
@@ -3,32 +3,101 @@ from os.path import isfile, join
 from scipy import fftpack, ndimage
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
 import sys
-
-np.set_printoptions(threshold=np.nan)
-def readTrainingSet():
-    paths = []
-    training = 'fruits-360/Training'
-    for className in listdir(training):
-        classDir = training + '/' + className
+import h5py
+import keras
+import tensorflow as tf
+import os.path
+import random
+from keras.models import load_model
+ 
+def readSet(dir):
+    files = []
+    for i, className in enumerate(listdir(dir)):
+        if className == '.DS_Store':
+            continue
+        ind = classesToIds[className]
+        classDir = dir + '/' + className
         for filename in listdir(classDir):
             path = classDir + '/' + filename
-            paths.append((path, className))
-    return paths
+            files.append((path, ind))
+    return getImages(files)
 
 def getImages(paths):
     images = []
-    for (path, className) in paths:
-        images.append((transformImage(ndimage.imread(path), className)))
+    for (path, classId) in paths:
+        images.append((transformImage(ndimage.imread(path)), classId))
     return images
 
-def transformImage(images):
-    result = np.zeros((100, 100, 6))
+def transformImage(image):
+    result_amplitude = np.zeros((100, 100, 6))
     for i in range(3):
         fft = np.fft.fft2(image[:, :, i])
         fshift = np.fft.fftshift(fft)
-        result[:, :, 2 * i] = fshift.real
-        result[:, :, 2 * i + 1] = fshift.imag
-    return result
+        result_amplitude[:, :, i] = np.abs(fshift) / 2445836.0
+        result_amplitude[:, :, 3+i] = np.angle(fshift) / 3.14
+    return result_amplitude
 
-print (readTrainingSet())
+
+def getClassesIds():
+    classesToIds = {}
+    idsToClasses = {}
+    for i, className in enumerate(listdir('fruits-360/Training')):
+        if className == '.DS_Store':
+            continue
+        if className not in classesToIds:
+            classesToIds[className] = i
+            idsToClasses[i] = className
+    return classesToIds, idsToClasses
+ 
+classesToIdsFile = 'classesToIds'
+idsToClassesFile = 'idsToClasses'
+ 
+if not os.path.exists(classesToIdsFile):
+    classesToIds, idsToClasses = getClassesIds()
+    with open(classesToIdsFile, 'wb') as handle:
+        pickle.dump(classesToIds, handle)
+    with open(idsToClassesFile, 'wb') as handle:
+        pickle.dump(idsToClasses, handle)
+ 
+with open(classesToIdsFile, 'rb') as handle:
+    classesToIds = pickle.load(handle)
+ 
+with open(idsToClassesFile, 'rb') as handle:
+    idsToClasses = pickle.load(handle)
+ 
+
+
+
+
+
+def generate_arrays_from_files(files2):
+    batch_features = np.zeros((batch_size, 100, 100, 6))
+    batch_labels = np.zeros((batch_size, 1))
+    while 1:
+        for i in range(batch_size):
+            index = random.choice(files2)
+            file, clas = index
+            batch_features[i] = file
+            batch_labels[i] = clas
+        yield (batch_features, batch_labels)
+
+files = readSet('fruits-360/Training')
+batch_size=100
+samples = np.ceil(len(files) / batch_size)
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(100, 100, 6)),
+    keras.layers.Dense(2048, activation=tf.nn.relu),
+    keras.layers.Dense(2048, activation=tf.nn.relu),
+    keras.layers.Dense(2048, activation=tf.nn.relu),
+    keras.layers.Dense(84, activation=tf.nn.softmax)
+])
+ 
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+ 
+model.fit_generator(generate_arrays_from_files(files), samples_per_epoch=samples*2, epochs=2000)
+model.save('my_model.h5')
